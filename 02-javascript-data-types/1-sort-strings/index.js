97dcdceba18939422e64428d6eccec7723a74089
Valery <nicemrlexx@live.ru>
2023-04-13 23:24:55 +0300

    1-sort-strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,31 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  let result = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    result[result.length] = arr[i];
+  }
+
+  for (let i = 0; i < result.length; i++) {
+    for (let y = i + 1; y < result.length; y++) {
+      const comp = result[i].localeCompare(result[y], 'ru', {'caseFirst': 'upper'});
+      if (param === 'asc' && comp > 0) {
+
+        const tmp = result[i];
+        result[i] = result[y];
+        result[y] = tmp;
+
+      } else if (param === 'desc' && comp < 0) {
+
+        const tmp = result[i];
+        result[i] = result[y];
+        result[y] = tmp;
+
+      }
+    }
+  }
+
+  return result;
 
 }
